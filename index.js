54f11ce93aaf49c3425aba9ebd446c4eb615406d
abrar0349 <brar0349khan@gmail.com>
2024-09-29 13:41:13 +0500

    I have completed todo list functionality Crud operation

--- index.js
+++ index.js
@@ -4,45 +4,52 @@ let todoList = document.getElementById('todoList');
 
 // console.log(inputBox,addBtn,todoList)
 
+let editTodo = null;
+
 addBtn.addEventListener('click',addTodo)
 
 function addTodo(){
     let inputText = inputBox.value.trim()
     // console.log(inputText)
-    // console.log(inputText.length <= 0)
     if(inputText.length <= 0){
         alert("please Enter the value");
         return false // this will stop the execution of bottom line
     }
-     
-    // create li Element 
-    const li = document.createElement("li");
-    const p = document.createElement("p")
-    p.innerHTML = inputText
-    li.appendChild(p)
-    todoList.appendChild(li)
-
-    // create delete button
-    const dltBtn = document.createElement("button")
-    dltBtn.innerHTML = "Remove"
-    dltBtn.classList.add("btn","deleteBtn")
-    li.appendChild(dltBtn)
-
-    // create edit button
-    const edtBtn = document.createElement("button")
-    edtBtn.innerHTML = "Edit"
-    edtBtn.classList.add("btn","editBtn")
-    li.appendChild(edtBtn)
    
+    if(addBtn.value == "Edit"){
+        // Rename the todo element
+        let target = editTodo.target.parentNode
+        let p = target.querySelector('p')
+        p.innerText = inputText
+        addBtn.value = "Add"
+     }else{
+        // create li Element 
+        const li = document.createElement("li");
+        const p = document.createElement("p")
+        p.innerHTML = inputText
+        li.appendChild(p)
+        todoList.appendChild(li)
+        // create delete button
+        const dltBtn = document.createElement("button")
+        dltBtn.innerHTML = "Remove"
+        dltBtn.classList.add("btn","deleteBtn")
+        li.appendChild(dltBtn)
+        // create edit button
+        const edtBtn = document.createElement("button")
+        edtBtn.innerHTML = "Edit"
+        edtBtn.classList.add("btn","editBtn")
+        li.appendChild(edtBtn)
+    }
 
     inputBox.value = "";
 }
 
+
 function updateTodo(e){
+
 if(e.target.innerHTML == "Remove"){
     console.log("Remove button is click")
     todoList.removeChild(e.target.parentElement)
-
 }
 
 if(e.target.innerHTML == "Edit"){
@@ -54,6 +61,9 @@ if(e.target.innerHTML == "Edit"){
     inputBox.value = p.innerHTML
     inputBox.focus()
     addBtn.value = "Edit";
+    editTodo = e
 }
+
 }
+
 todoList.addEventListener('click',updateTodo)
