a9f63f30505b089f4c4e0140a278b462cc608839
abrar0349 <brar0349khan@gmail.com>
2024-09-30 14:26:17 +0500

    Today I also perform Crud operation but this Time I perform crud operation on localStorage which is use for storing the data

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ let todoList = document.getElementById('todoList');
 
 let editTodo = null;
 
-addBtn.addEventListener('click',addTodo)
-
+// Update and add todo list data
 function addTodo(){
     let inputText = inputBox.value.trim()
-    // console.log(inputText)
     if(inputText.length <= 0){
         alert("please Enter the value");
         return false // this will stop the execution of bottom line
@@ -20,6 +18,7 @@ function addTodo(){
         // Rename the todo element
         let target = editTodo.target.parentNode
         let p = target.querySelector('p')
+        editDataFromLocalStorage(p)
         p.innerText = inputText
         addBtn.value = "Add"
      }else{
@@ -39,17 +38,21 @@ function addTodo(){
         edtBtn.innerHTML = "Edit"
         edtBtn.classList.add("btn","editBtn")
         li.appendChild(edtBtn)
+
+        saveLocalStorage(inputText) // For storing data in Local storage
     }
 
     inputBox.value = "";
 }
+addBtn.addEventListener('click',addTodo)
 
-
-function updateTodo(e){
+// Delete todo list data
+function deleteTodo(e){
 
 if(e.target.innerHTML == "Remove"){
     console.log("Remove button is click")
     todoList.removeChild(e.target.parentElement)
+    deleteDataFromLocalStorage(e)
 }
 
 if(e.target.innerHTML == "Edit"){
@@ -65,5 +68,73 @@ if(e.target.innerHTML == "Edit"){
 }
 
 }
+todoList.addEventListener('click',deleteTodo)
 
-todoList.addEventListener('click',updateTodo)
+// Save todos data in localStorage
+const saveLocalStorage = (todo) => {
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = []
+    }else{
+        todos =  JSON.parse(localStorage.getItem('todos'))
+    }
+    todos.push(todo)
+    localStorage.setItem('todos',JSON.stringify(todos))  // In there when I use undefine value then it's give me error on line no 79 and 43
+    console.log(todos)
+   
+}
+
+//Get data from localstorage and render it my dom load
+const getDataFromLocalStorage = () => {
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = []
+    }else{
+        todos =  JSON.parse(localStorage.getItem('todos'))
+    }
+    todos.forEach( (todo) => {
+        // create li Element 
+        const li = document.createElement("li");
+        const p = document.createElement("p")
+        p.innerHTML = todo
+        li.appendChild(p)
+        todoList.appendChild(li)
+
+        // create delete button
+        const dltBtn = document.createElement("button")
+        dltBtn.innerHTML = "Remove"
+        dltBtn.classList.add("btn","deleteBtn")
+        li.appendChild(dltBtn)
+
+        // create edit button
+        const edtBtn = document.createElement("button")
+        edtBtn.innerHTML = "Edit"
+        edtBtn.classList.add("btn","editBtn")
+        li.appendChild(edtBtn)
+    })
+}
+document.addEventListener('DOMContentLoaded',getDataFromLocalStorage)
+
+//function to delete todo list data
+const deleteDataFromLocalStorage = (todo) => {
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+    console.log()
+    let todoText = todo.target.parentNode.querySelector('p').innerHTML; // I face issue -1 becuase I didn't give .innerHtml
+    let findIndex = todos.indexOf(todoText)
+    todos.splice(findIndex , 1)
+    // console.log(findIndex)
+    localStorage.setItem('todos',JSON.stringify(todos))
+}
+
+const editDataFromLocalStorage = (todo) => {
+    // console.log(todo)
+    let todos = JSON.parse(localStorage.getItem('todos'))
+    let indexNo = todos.indexOf(todo.innerHTML)
+    todos[indexNo] = inputBox.value;
+    localStorage.setItem('todos',JSON.stringify(todos))
+}
